86b84fc27c4c36b7a75baac338aeda64e508a033
graevsky <graevsky@bk.ru>
2025-05-03 13:14:36 +0300

    Reformat files

--- app/src/main/kotlin/com/training/workout/model/Exercise.kt
+++ app/src/main/kotlin/com/training/workout/model/Exercise.kt
@@ -1,23 +1,23 @@
 package com.training.workout.model
 
-import kotlinx.serialization.Serializable
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
+import kotlinx.serialization.Serializable
 
 @Serializable
 data class Exercise(
-  val id: String = "",
-  val name: ExerciseName = ExerciseName.EXERCISE_NAME_UNSPECIFIED,
-  val duration: Duration = 0.seconds,
-  val exerciseType: ExerciseType = ExerciseType.EXERCISE_TYPE_UNSPECIFIED,
-  val sets: Int? = null,
-  val reps: Int? = null,
-  val distance: Int? = null,
-  val steps: Int? = null,
-  val bmp: Int? = null,
-  val speed: Int? = null,
-  val weight: Int? = null,
-  val calories: Double? = null,
-  val reaction: ExerciseReaction? = null,
-  val note: String? = null
-  )
+    val id: String = "",
+    val name: ExerciseName = ExerciseName.EXERCISE_NAME_UNSPECIFIED,
+    val duration: Duration = 0.seconds,
+    val exerciseType: ExerciseType = ExerciseType.EXERCISE_TYPE_UNSPECIFIED,
+    val sets: Int? = null,
+    val reps: Int? = null,
+    val distance: Int? = null,
+    val steps: Int? = null,
+    val bmp: Int? = null,
+    val speed: Int? = null,
+    val weight: Int? = null,
+    val calories: Double? = null,
+    val reaction: ExerciseReaction? = null,
+    val note: String? = null
+)
